e8787ea1471974991143d4ad4b4cf2e5c13e5533
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-06 23:29:35 +0530

    Added DepInjectionLauncherApplication and SimpleSpringContextLauncher for exploring Dependency Injection

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learn_spring_framework.examples.a1;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan // Automatically scans the current package and its sub-packages for @Component classes
+public class DepInjectionLauncherApplication {
+    public static void main(String[] args){
+        try(var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class))
+        {
+           Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
