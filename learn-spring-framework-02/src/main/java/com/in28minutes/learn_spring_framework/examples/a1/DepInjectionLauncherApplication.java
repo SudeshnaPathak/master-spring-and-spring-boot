16f40a9654201dcad5c0b35bc75594122bb1c493
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-07 00:22:02 +0530

    Add YourBusinessClass and dependencies with field injection in DepInjectionLauncherApplication

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -1,9 +1,31 @@
 package com.in28minutes.learn_spring_framework.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
+
+@Component
+class YourBusinessClass{
+    //Field Injection : No Setter or Constructor needed, Dependency is injected using Reflection
+    @Autowired // When we place autowired over a field, Spring searches for a bean of type Dependency1 and injects it here
+   Dependency1 dependency1;
+    @Autowired
+   Dependency2 dependency2;
+
+   public String toString(){
+       return "Using "+ dependency1 + " and " + dependency2;
+   }
+}
+
+@Component
+class Dependency1{ }
+
+@Component
+class Dependency2{ }
 
 @Configuration
 @ComponentScan // Automatically scans the current package and its sub-packages for @Component classes
@@ -12,6 +34,7 @@ public class DepInjectionLauncherApplication {
         try(var context = new AnnotationConfigApplicationContext(DepInjectionLauncherApplication.class))
         {
            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+           System.out.println(context.getBean(YourBusinessClass.class));
         }
     }
 }
