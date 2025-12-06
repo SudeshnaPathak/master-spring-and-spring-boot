ce816d0ec001f38fd0ded7485afd4c732517c6cc
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-06 21:33:27 +0530

    Getting Spring Framework to create beans using @Component, thereby automatically creating and managing our java objects

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/GamingAppLauncherApplication.java
@@ -3,23 +3,17 @@ package com.in28minutes.learn_spring_framework;
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.in28minutes.learn_spring_framework.game") // To scan for @Component classes in the specified package
-public class App03GamingSpringBeans {
-
-    @Bean
-    public GameRunner gameRunner(GamingConsole game)
-    {
-        // Auto-wiring by Spring Framework - it will search for a bean of type GamingConsole using @ComponentScan and inject it here
-        return new GameRunner(game);
-    }
+public class GamingAppLauncherApplication {
     public static void main(String[] args) {
-       try(var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class))
+       try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class))
        {
+           // Auto-wiring by Spring Framework - it will search for beans of type GamingConsole and type GameRunner using @ComponentScan and inject it here
+           // Spring Scans the right Package -> finds the classes with @Component -> creates the instances -> Auto Wires the instances -> Gets the application up and running
            context.getBean(GamingConsole.class).up();
            context.getBean(GameRunner.class).run();
        }
