1ae69c06c39a0d1264500ee43156e70ebb054824
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-06 20:34:15 +0530

    Remove seperate GamingConfiguration class, use App03GamingSpringBeans as the Configuration class and define beans inside it.

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -2,11 +2,27 @@ package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import com.in28minutes.learn_spring_framework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+    @Bean
+    public GamingConsole game()
+    {
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game)
+    {
+        // Auto-wiring by method parameter
+        return new GameRunner(game);
+    }
     public static void main(String[] args) {
-       try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class))
+       try(var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class))
        {
            context.getBean(GamingConsole.class).up();
            context.getBean(GameRunner.class).run();
