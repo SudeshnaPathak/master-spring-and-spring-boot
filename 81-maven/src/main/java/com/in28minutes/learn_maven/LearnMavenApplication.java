fe9a1627727d7f4445f9f9eb63f44dd84064c203
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-08 10:37:41 +0530

    include spring-boot-starter-web dependency in pom.xml

--- 81-maven/src/main/java/com/in28minutes/learn_maven/LearnMavenApplication.java
+++ 81-maven/src/main/java/com/in28minutes/learn_maven/LearnMavenApplication.java
@@ -11,3 +11,14 @@ public class LearnMavenApplication {
 	}
 
 }
+
+//2 popular java Build Tools:
+//Maven - Manages Project build , reporting & documentation
+//Gradle
+//Use of Maven:
+//Create new Projects
+//Manage dependencies(Frameworks & Libraries made use of in the project) - Downloads the dependencies & the transitive dependencies from POM.Xml, makes the jars available for the project
+//Build a JAR File
+//Run Applications on TomCat or jetty
+//Run Unit Tests
+//Deploy to a Test Environments
