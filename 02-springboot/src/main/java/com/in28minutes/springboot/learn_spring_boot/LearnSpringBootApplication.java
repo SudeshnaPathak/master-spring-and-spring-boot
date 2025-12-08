0d2d73a5a6dcdb7991cbd4d8f2298bd512e15056
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-08 19:07:23 +0530

    Add comments to application.properties and LearnSpringBootApplication for clarity on logging and Spring Boot features

--- 02-springboot/src/main/java/com/in28minutes/springboot/learn_spring_boot/LearnSpringBootApplication.java
+++ 02-springboot/src/main/java/com/in28minutes/springboot/learn_spring_boot/LearnSpringBootApplication.java
@@ -11,3 +11,7 @@ public class LearnSpringBootApplication {
 	}
 
 }
+
+//Springboot Starter Projects - A collection of all the dependencies you need to build a specific type of application. For example, spring-boot-starter-web for web applications, spring-boot-starter-data-jpa for JPA-based data access, etc.
+//Spring Boot Auto Configuration - Automatically configures DispatcherServelet , Embedded Servers (Tomcat, Jetty) , DataSource , JPA , Default Error Pages , Bean<->JSON Mapping , etc. based on the dependencies present in the classpath.
+//Present in Spring Boot AutoConfigure jar
