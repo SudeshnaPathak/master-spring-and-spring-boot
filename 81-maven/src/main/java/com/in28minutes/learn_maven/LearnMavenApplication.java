4670516f7ae586ed49636460cd6eb82b2adbc91e
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-08 13:23:03 +0530

    Add detailed comments to LearnMavenApplication for better understanding of Maven concepts

--- 81-maven/src/main/java/com/in28minutes/learn_maven/LearnMavenApplication.java
+++ 81-maven/src/main/java/com/in28minutes/learn_maven/LearnMavenApplication.java
@@ -25,3 +25,25 @@ public class LearnMavenApplication {
 
 //From Parent pom : spring-boot-starter-parent
 // We inherit spring boot dependencies , java version , plugin management , configurations
+
+//GroupId : Similar to package name , uniquely identifies your project across all projects
+//ArtifactId : Similar to class name , uniquely identifies your project within the group
+//Version : version of the project
+
+//Once we push our dependencies to a Maven Repository , other projects can make use of these dependencies by using our GroupId , ArtifactId & Version
+
+//Maven Central Repository : Contains all the jars indexed by GroupId , ArtifactId & Version
+//When a dependency is added in the POM.xml file , Maven checks in the Central Repository for the jar
+//These Jars are downloaded from Maven Central Repository to our Maven Local Repository
+//In our project , we are making use of the jars from the Maven Local Repository
+//Maven Local Repository : A temp folder on our machine where maven stores all the jars & dependency files downloaded from the Central Repository
+
+//mnv commands:
+// mvn --version : to check maven version
+// mvn compile : to compile source files
+// mvn test-compile : to compile test files(also compiles source files if not already compiled)
+// mvn clean : to clean the target folder
+// mvn test : to run tests(also compiles source & test files if not already compiled)
+// mvn package : to create a jar file(also runs tests, compiles source & test files if not already done)
+// mvn dependency:tree : to view dependency tree of the project
+// mvn help:effective-pom : to view the effective pom including inherited dependencies & plugins from parent pom
