e799d236a725d66df20d7c78fcd5a63ca67b52b3
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-08 17:19:20 +0530

    Add Course and CourseController classes for course management using RestAPI that returns a list of Courses[id , name , author]

---
+++ 02-springboot/src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController // to define a controller and to handle HTTP requests
+public class CourseController {
+
+    @RequestMapping("/courses") // to map HTTP requests to handler methods of MVC and REST controllers
+    public List<Course> retrieveAllCourse()
+    {
+        return Arrays.asList(
+                new Course(1 , "Learn AWS" , "in28Minutes"),
+                new Course(2 , "Learn DevOps" , "in28Minutes")
+        );
+    }
+}
