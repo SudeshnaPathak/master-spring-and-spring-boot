203229442bd5094532c45b1a9959beba7e80583a
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-10 12:46:21 +0530

    Add sayHelloHtml method to SayHelloController for HTML response

--- 11-web-application/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ 11-web-application/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -6,11 +6,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // To define a controller which can handle HTTP requests
 public class SayHelloController {
-    //say-hello -> "Hello! What are you learning today?"
 
     @RequestMapping("say-hello")
     @ResponseBody // To indicate that the return value should be used as the response body
     public String sayHello() {
         return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My First HTML Page - Changed</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My First HTML Page with Body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
     }
 }
