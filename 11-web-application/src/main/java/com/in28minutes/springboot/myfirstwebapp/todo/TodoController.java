20a4f0c0f0477366f6179d43aabb0cf2fa3e0376
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-11 19:28:29 +0530

    Implementing Add Todo functionality, Add Todo button in ListTodos page redirecting to Add Todo Page which takes todo description as input ,adds a new Todo to the static Todo List, redirects back to ListTodos page

--- 11-web-application/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ 11-web-application/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,8 +3,11 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -23,4 +26,17 @@ public class TodoController {
         model.addAttribute("todos" , todos);
         return "listTodos";
     }
+
+    @RequestMapping(value = "add-todo" , method = RequestMethod.GET)
+    public String showNewTodoPage(ModelMap model) {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo" , method = RequestMethod.POST)
+    public String addNewTodo(@RequestParam String description , ModelMap model) {
+        String username = model.get("name").toString(); //Getting the username from the Session Attribute
+        todoService.addTodo(username , description , LocalDate.now().plusYears(1) , false);
+        return "redirect:list-todos";
+    }
+
 }
