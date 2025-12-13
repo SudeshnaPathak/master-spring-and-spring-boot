20c703a87968ae11eb2f6b598949ba8f9d7d6ef1
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-13 17:08:40 +0530

    Implementing GET Methods for User Resource

---
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,27 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private final UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+    //GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    //GET /users/1
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
