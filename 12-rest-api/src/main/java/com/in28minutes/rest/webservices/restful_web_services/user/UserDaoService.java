1f60bb1e457b4be7033e5737e0d8b8338ef6221c
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-13 16:20:28 +0530

    Add User model and UserDaoService for user management

---
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    //Data Access Object (DAO) or Repository
+    //Later JPA or Hibernate > Database
+    //For now, UserDaoService > Static List of Users
+
+    // public User save(User user)
+    // public User findOne(int id)
+
+    private static List<User> users = new ArrayList<>();
+
+    static{
+        users.add(new User(1 , "Adam" , LocalDate.now().minusYears(30)));
+        users.add(new User(2 , "Eve" , LocalDate.now().minusYears(25)));
+        users.add(new User(3 , "Jim" , LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+}
