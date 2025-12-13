ecbd0d1b013ea008fc7b22c1149afa1c26818043
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-13 14:50:30 +0530

    Accessing Path parameters using @PathVariable

--- 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContoller.java
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContoller.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,12 @@ public class HelloWorldContoller {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    //Path Parameters
+    //hello-world/path-variable/Sudeshna
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
