acb245130cce9bba7a318a567f9b73dfc90c9034
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-14 14:58:14 +0530

    Add internationalization support using messages.properties & locale sent in Accept Language Header by the client

--- 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContoller.java
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContoller.java
@@ -1,11 +1,21 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldContoller {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldContoller(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -24,5 +34,12 @@ public class HelloWorldContoller {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    //Internationalization
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale(); //gets the locale of the request based on Accept-Language Header sent by the client
+        return messageSource.getMessage("good.morning.message", null, "Default Message" ,locale);
     }
 }
