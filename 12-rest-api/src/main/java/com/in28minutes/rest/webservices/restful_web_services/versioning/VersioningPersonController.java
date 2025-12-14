8484d8a201e44851b8e17ee01c37c2483dd84e08
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-14 20:48:37 +0530

    Versioning ~ Request Params, Header, Content Negotiation using accept header

--- 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ 12-rest-api/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -5,12 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
-    //Version APIs for Breaking Changes
-    //Versioning Types:
-    //1. URI Versioning
-    //2. Request Parameter Versioning
-    //3. Header Versioning
-    //4. Media Type Versioning (a.k.a Content Negotiation or Accept Header Versioning)
 
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
@@ -21,4 +15,46 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPerson() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path="/person" , params="version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path="/person", params="version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path="/person/header" , headers="X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path="/person/header" , headers="X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    @GetMapping(path="/person/acceptheader" , produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path="/person/acceptheader" , produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
+
+//Version APIs for Breaking Changes
+//Versioning Types:
+
+//Can be easily executed in Browser , URI Pollution , Good for Caching
+//Good for API Documentation
+//1. URI Versioning ~Twitter
+//2. Request Parameter Versioning ~Amazon
+
+// Bad for Caching & Misuse of Headers for Versioning
+//3. Header Versioning ~Microsoft
+//4. Media Type Versioning (a.k.a Content Negotiation or Accept Header Versioning) ~GitHub
