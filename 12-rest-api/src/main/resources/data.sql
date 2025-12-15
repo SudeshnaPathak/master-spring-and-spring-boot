3f14433412695524fd214c9fa7c5d1e803902fc9
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-15 12:18:35 +0530

    Enable H2 console, configure datasource, add initial user data and create User Entity

---
+++ 12-rest-api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details (id, name, birth_date)
+VALUES (10001, 'Sudeshna', current_date());
+
+INSERT INTO user_details (id, name, birth_date)
+VALUES (10002, 'Jolly', current_date());
+
+INSERT INTO user_details (id, name, birth_date)
+VALUES (10003, 'Joy', current_date());
