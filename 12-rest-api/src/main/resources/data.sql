e56e2795eb816443ac12a66bf0a1dc4afa9875c0
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-16 10:59:37 +0530

    Add Post entity with many-to-one relationship with user entity, mapped by user to ensure user_id feild in post table, insert sample posts into data.sql

--- 12-rest-api/src/main/resources/data.sql
+++ 12-rest-api/src/main/resources/data.sql
@@ -6,3 +6,15 @@ VALUES (10002, 'Jolly', current_date());
 
 INSERT INTO user_details (id, name, birth_date)
 VALUES (10003, 'Joy', current_date());
+
+INSERT INTO post(id , description , user_id)
+VALUES (20001, 'I want to learn AWS', 10001);
+
+INSERT INTO post(id , description , user_id)
+VALUES (20002, 'I want to learn DevOps', 10001);
+
+INSERT INTO post(id , description , user_id)
+VALUES (20003, 'I want to learn Docker', 10002);
+
+INSERT INTO post(id , description , user_id)
+VALUES (20004, 'I want to learn Kubernetes', 10002);
