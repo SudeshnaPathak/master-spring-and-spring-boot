9ece90f29d983f47e9e02d6156ad459baa31bac5
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-20 17:50:37 +0530

    Add comments to MyMath.java explaining unit testing and its advantages

--- 51-junit/src/com/in28minutes/junit/MyMath.java
+++ 51-junit/src/com/in28minutes/junit/MyMath.java
@@ -10,3 +10,17 @@ public class MyMath {
         return sum;
     }
 }
+
+
+//Testing : Checking application behavior against expected behavior
+//Option 1: Deploy the complete application and test
+//->System Testing or Integration Testing
+//Option 2:Test specific units of the application code independently
+//i.e. test a specific method or a group of methods or an entire class-> Unit Testing
+
+//ADVANTAGES OF UNIT TESTING
+//~ Find bugs early(run under CI)
+//~ Easy to fix bugs
+//~ Reduces cost in Long Run
+
+//POPULAR JAVA FRAMEWORKS ~ JUNIT & MOCKITO
