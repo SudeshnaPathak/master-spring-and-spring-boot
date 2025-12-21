b0c71e1e70a526e81dcc5ba5ca8a701b968f8515
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-21 23:36:38 +0530

    Add Counter.css for styling the Counter component

--- 13-full-stack/01-FRONTEND/todo-app/src/components/counter/Counter.jsx
+++ 13-full-stack/01-FRONTEND/todo-app/src/components/counter/Counter.jsx
@@ -1,16 +1,39 @@
+import './Counter.css';
+
 export default function Counter()
 {
     function incrementCounterFunction()
     {
-        console.log("Increment Counter Clicked");
+        console.log("Increment Counter Clicked")
     }
+
+    // const buttonStyle = {
+    //     'fontSize' : '16px',
+    //     'backgroundColor' : '#00a5ab',
+    //     'width' : '100px',
+    //     'margin' : '10px',          
+    //     'color' : 'white',
+    //     'padding' : '15px',
+    //     'borderRadius' : '30px'
+    // }
 
     return (
         <div className="Counter">
             <span className="count">0</span> 
             <div>
-                <button className="counterButton" onClick={incrementCounterFunction}> +1 </button> {/* If we use parentheses here like incrementCounterFunction() , the function will be called during the rendering itself which is not what we want. We want to call it only when the button is clicked. */}
+                <button className="counterButton" 
+                onClick={incrementCounterFunction}
+                // style = {buttonStyle}
+                >+1 </button>
             </div>
         </div>
     );
 }
+
+//  If we use parentheses here like incrementCounterFunction() , the function will be called during the rendering itself which is not what we want. We want to call it only when the button is clicked.
+
+// Two options to style in React:
+// 1. Using CSS file (as done here) - Counter.css 
+// 2. Using inline styles 
+
+// style attribute in JSX takes a JavaScript object instead of a string as in HTML. Hence we define buttonStyle as an object and pass it to the style attribute.
