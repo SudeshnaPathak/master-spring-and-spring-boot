73df1d09857ace04643c7d1d9e615be5b9d17506
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-23 15:18:18 +0530

    Add TodoApp styling and implement controlled components for username and password inputs in login form

--- 13-full-stack/01-FRONTEND/todo-app/src/components/todo/TodoApp.jsx
+++ 13-full-stack/01-FRONTEND/todo-app/src/components/todo/TodoApp.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import './TodoApp.css'
 export default function TodoApp() {
     return(
         <div className="TodoApp">
@@ -9,16 +11,26 @@ export default function TodoApp() {
 }
 
 function LoginComponent(){
+    const [username , setUsername] = useState('')
+    const [password , setPassword] = useState('')
+
+    function handleUsername(event){
+        setUsername(event.target.value)
+    }
+    function handlePassword(event){
+        setPassword(event.target.value)
+    }
+
     return(
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username"/>
+                    <input type="text" name="username" value={username} onChange={handleUsername}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" value={password} onChange={handlePassword}/>
                 </div>
                 <div>
                     <button type="button" name="Login">Login</button>
@@ -27,6 +39,11 @@ function LoginComponent(){
         </div>
     )
 }
+
+//Controlled Component - Input feild whose value is controlled by React state variable
+//Whenever something changes in the input feild , onChange event is fired
+//and event handler updates the state variable
+//whenever state variable is updated , the feild element is also updated
 
 // function WelcomeComponent(){
 //     return(
