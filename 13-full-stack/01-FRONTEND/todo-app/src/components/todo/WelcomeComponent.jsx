31171ea1b0588b354802b8623cde8ebefe045797
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-27 23:20:52 +0530

    Add CORS configuration and integrate Axios for API calls in WelcomeComponent

--- 13-full-stack/01-FRONTEND/todo-app/src/components/todo/WelcomeComponent.jsx
+++ 13-full-stack/01-FRONTEND/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,35 @@
+import axios from 'axios'
 import {useParams, Link } from 'react-router-dom'
 
 export default function WelcomeComponent(){
 
     const {username} = useParams() //hook to get path variables, object is destructured to get username
     
+    function callHelloWorldRestAPI(){
+        console.log("called")
+        //axios to call REST API
+        axios.get('http://localhost:8080/hello-world')          //Returns a Promise on a GET request to the specified URL
+             .then((response) => successfulResponse(response))  //Callback for successful response
+             .catch((error) => errorResponse(error))            //Callback for error response
+             .finally(() => console.log('cleanup'))             //Callback for cleanup activity ~ always executed
+    }
+
+    function successfulResponse(response){
+        console.log(response)
+    }
+
+    function errorResponse(error){
+        console.log(error)
+    }
+
     return(
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
                 Manage your Todos <Link to="/todos">here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestAPI}>Call Hello World</button>
             </div>
         </div>
     )
--- 13-full-stack/02-REST-API/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/RestfulWebServicesApplication.java
+++ 13-full-stack/02-REST-API/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/RestfulWebServicesApplication.java
@@ -2,6 +2,9 @@ package com.in28minutes.rest.webservices.restful_web_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +13,20 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	//http://localhost:3000/ to 8080
+    //Cross Origin Requests are blocked by default by browsers.
+    //Allow all Requests from http://localhost:3000/
+	//Define a Bean to configure CORS settings
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+			// Override the addCorsMappings method of WebMvcConfigurer to allow CORS requests
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") //Add mapping to allow CORS requests to all endpoints
+						.allowedMethods("*") //Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+						.allowedOrigins("http://localhost:3000"); //Allow requests from this origin
+			}
+        };
+    }
 }
+
