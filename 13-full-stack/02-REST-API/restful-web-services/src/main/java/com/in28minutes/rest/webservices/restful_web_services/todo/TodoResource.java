ac48b4ad3ed8b6f359e6177a349322855ef693fe
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-28 17:50:37 +0530

    Add TodoResource RestController to handle retrieval of todos for a specific user

---
+++ 13-full-stack/02-REST-API/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/todo/TodoResource.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restful_web_services.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    private final TodoService todoService;
+
+    public TodoResource(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public List<Todo> retrieveTodos(@PathVariable String username) {
+        return todoService.findByUsername(username);
+    }
+}
