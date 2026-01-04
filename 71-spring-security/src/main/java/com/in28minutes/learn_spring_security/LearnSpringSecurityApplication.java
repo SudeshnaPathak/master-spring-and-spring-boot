5599573946a579db8263825afe49331b0ccabc46
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-01-04 19:42:00 +0530

    Add CORS configuration to allow requests from localhost:3000

--- 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/LearnSpringSecurityApplication.java
+++ 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/LearnSpringSecurityApplication.java
@@ -2,6 +2,9 @@ package com.in28minutes.learn_spring_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LearnSpringSecurityApplication {
@@ -10,7 +13,25 @@ public class LearnSpringSecurityApplication {
 		SpringApplication.run(LearnSpringSecurityApplication.class, args);
 	}
 
+	//http://localhost:3000/ to 8080
+	//Cross Origin Requests are blocked by default by browsers.
+	//Allow all Requests from http://localhost:3000/
+	//Define a Bean to configure CORS settings
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			// Override the addCorsMappings method of WebMvcConfigurer to allow CORS requests
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") //Add mapping to allow CORS requests to all endpoints
+						.allowedMethods("*") //Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+						.allowedOrigins("http://localhost:3000"); //Allow requests from this origin
+			}
+		};
+	}
+
 }
+
+
 
 //Spring Security - Learn Spring Security
 //1.By Default, Everything is authenticated
@@ -26,7 +47,12 @@ public class LearnSpringSecurityApplication {
 	//Sends Base64 encoded username and password in the Authorization header
 	//Not recommended for production , it has many flaws
 
+//4.CSRF Protection
 //CSRF Protection is by default enabled in Spring Security
 //Spring Security allows READ Requests for Basic Authentication, However, Spring Security requires a CSRF token for WRITE Requests (POST, PUT, DELETE) which modifies the state of the server
 //Synchronizer Token Pattern is used , A token is created for each request , To make an update(POST, PUT, DELETE), the client must send the token along with the request
 //For Stateless Applications (REST APIs), we can disable CSRF protection in Spring Security Configuration
+
+//5.CORS Configuration
+//Browsers do not allow AJAX calls to resources residing outside the current origin
+//CORS - Cross Origin Resource Sharing allow us to configure which cross domain requests are allowed
