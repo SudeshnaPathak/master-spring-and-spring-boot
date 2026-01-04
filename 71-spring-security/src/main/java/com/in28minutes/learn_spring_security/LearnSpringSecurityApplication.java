36a67ce9ef7b0306b41921c14dfff1a790b49ba1
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-01-04 13:13:34 +0530

    Add HelloWorldResource with a simple hello world endpoint and added comments for Form Based Authentication

--- 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/LearnSpringSecurityApplication.java
+++ 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/LearnSpringSecurityApplication.java
@@ -11,3 +11,13 @@ public class LearnSpringSecurityApplication {
 	}
 
 }
+
+//Spring Security - Learn Spring Security
+//1.By Default, Everything is authenticated
+//2.Form Based Authentication is Enabled
+	//Form Based Authentication is used by most web applications
+	//Uses a login form to collect username and password
+	//Uses sessions and cookies to remember the logged in user
+	//Eg: ajs_anonymous_id=ec8abce9-cc7d-4343-88b8-f4d4de8aa1c2; JSESSIONID=5A31DE5732C191CC8FE163631A61F4AA
+	//This session id is sent as a cookie to identify the user for all subsequent requests
+	//Provides default login & logout pages through /login & /logout endpoints
