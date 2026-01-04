cf65f84cbabfeb71617ad6a42daff55bb4ce3a17
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-01-04 21:07:22 +0530

    Store User Credentials in an In Memory Database with roles for basic authentication

--- 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -40,4 +44,20 @@ public class BasicAuthSecurityConfiguration {
         return http.build();
 
     }
+
+    //Storing User Credentials in In-Memory Database(Not Recommended for Production)
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("Sudeshna")
+                .password("{noop}dummy") //{noop} means NoOpPasswordEncoder is used which does not perform any encoding
+                .roles(Role.USER.name())
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles(Role.ADMIN.name())
+                .build();
+        return new InMemoryUserDetailsManager(user , admin);
+    }
+
 }
---
+++ 71-spring-security/src/main/java/com/in28minutes/learn_spring_security/basic/Role.java
@@ -0,0 +1,6 @@
+package com.in28minutes.learn_spring_security.basic;
+
+public enum Role {
+    USER,
+    ADMIN
+}
