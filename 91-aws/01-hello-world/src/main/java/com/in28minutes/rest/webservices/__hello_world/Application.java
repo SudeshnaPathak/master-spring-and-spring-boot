ef3ba781adc4f1a89ae6d40ed346e8883dae48b9
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-01-11 23:47:01 +0530

    Add deployment instructions for Elastic Beanstalk to Application.java

--- 91-aws/01-hello-world/src/main/java/com/in28minutes/rest/webservices/__hello_world/Application.java
+++ 91-aws/01-hello-world/src/main/java/com/in28minutes/rest/webservices/__hello_world/Application.java
@@ -11,3 +11,14 @@ public class Application {
 	}
 
 }
+
+//Deployment to Elastic Beanstalk:
+//Create a JAR file using Maven or Gradle.
+//Go to Elastic Beanstalk in the AWS Management Console.
+//Create a new application
+//Create a new environment (Web server environment).
+//Configure application name, platform (Java), use a corretto version which >= your JDK version.
+//Upload the JAR file.
+//Add a version label after uploading the JAR file to track deployments.
+//Launch the application.
+//Access the application using the provided URL from Elastic Beanstalk.
