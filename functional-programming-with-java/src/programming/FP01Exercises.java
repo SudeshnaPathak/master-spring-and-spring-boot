a30b32bfad168cdfaab027759d61f1498ca51489
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-01-18 16:31:18 +0530

    Using map in Functional Programs

--- functional-programming-with-java/src/programming/FP01Exercises.java
+++ functional-programming-with-java/src/programming/FP01Exercises.java
@@ -27,6 +27,23 @@ public class FP01Exercises {
         Courses.stream()
                 .filter(course -> course.length() >= 4)
                 .forEach(System.out::println);
+
+        System.out.println("Exercise 5: Print the square of even numbers");
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * n) //map is used to transform each element
+                .forEach(System.out::println);
+
+        System.out.println("Exercise 6: Print the cube of odd numbers");
+        numbers.stream()
+                .filter(n -> n % 2 == 1)
+                .map(n -> Math.pow(n , 3))
+                .forEach(System.out::println);
+
+        System.out.println("Exercise 7: Print the length of each course name");
+        Courses.stream()
+                .map(course -> course + " " + course.length())
+                .forEach(System.out :: println);
     }
 
 }
